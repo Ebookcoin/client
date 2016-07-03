7c76f16eb3bdb6dfe925421050e0a01101d626b6
imfly <kubying@foxmail.com>
2016-07-03 23:47:50 +0800

    Update testnet time.

--- js/filters/timeAgoFilter.js
+++ js/filters/timeAgoFilter.js
@@ -5,7 +5,7 @@ angular.module('ebookcoinApp').filter('timeAgoFilter', function ($filter) {
         if (fullTime) {
             return $filter('timestampFilter')(time);
         }
-        var d = new Date(Date.UTC(2015, 3, 9, 0, 0, 0, 0));
+        var d = new Date(Date.UTC(2016, 5, 20, 0, 0, 0, 0));
         var t = parseInt(d.getTime() / 1000);
 
         time = new Date((time + t) * 1000);
--- js/filters/timestampFilter.js
+++ js/filters/timestampFilter.js
@@ -3,7 +3,7 @@ require('angular');
 angular.module('ebookcoinApp').filter('timestampFilter', function () {
     return function (timestamp) {
         // Epoch time
-        var d = new Date(Date.UTC(2015, 3, 9, 0, 0, 0, 0));
+        var d = new Date(Date.UTC(2016, 5, 20, 0, 0, 0, 0));
         var t = parseInt(d.getTime() / 1000);
 
         var d = new Date((timestamp + t) * 1000);
